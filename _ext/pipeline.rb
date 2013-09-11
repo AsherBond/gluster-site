366cd6550cbec47a0a1a8646600a83980425a45d
Tuomas Kuosmanen <tigert@redhat.com>
2013-09-11 14:37:42 +0300

    somehow I poked something and it broke, then I poked something else and it worked again. duh.

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,10 +1,20 @@
 require 'bootstrap-sass'
+require 'readmore'
+require 'erubis'
+require 'tilt'
 
 Awestruct::Extensions::Pipeline.new do
-  # extension Awestruct::Extensions::Posts.new '/news'
+  extension Awestruct::Extensions::Posts.new( '/blog')
+#  extension Awestruct::Extensions::Paginator.new( :posts, '/index', :per_page => 5 )
+#  extension Awestruct::Extensions::Tagger.new( :posts, '/index', '/blog/tags', :per_page => 10)
+#  extension Awestruct::Extensions::TagCloud.new( :tagcloud, '/blog/tags/index.html', :layout=>'base', :title=>'Tags')
+#  extension Awestruct::Extensions::Disqus.new
+
   extension Awestruct::Extensions::Indexifier.new
-  # Indexifier *must* come before Atomizer
-  extension Awestruct::Extensions::Atomizer.new :posts, '/feed.atom'
-  # Partials are like includes :)
+  extension Awestruct::Extensions::Atomizer.new( :posts, '/feed.atom', :feed_title=>'Gluster' )
+
   helper Awestruct::Extensions::Partial
+#  helper Awestruct::Extensions::GoogleAnalytics
+#  helper Awestruct::Extensions::ReadMore
 end
+
